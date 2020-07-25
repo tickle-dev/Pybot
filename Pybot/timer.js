87746fac310d1dd2ffdcacfd1dabb9210c615633
Drew Dewitt <drewpdewitt@gmail.com>
2020-07-25 07:54:39 -0400

    moved folder

---
+++ Pybot/timer.js
@@ -0,0 +1,17 @@
+
+function SetTimer(myGuild){
+var Two_Hour_Message = setTimeout(function(){ 
+    sendMessage(myGuild); // send the message once
+    var dayMillseconds = 1000 * 60 * 60 * 2; // math for milliseconds to 2 hours
+    setInterval(function(){ // repeat this every 2 hours
+        sendMessage(myGuild);
+    }, dayMillseconds)
+});
+}
+function sendMessage(myGuild){
+
+    myGuild.channels.cache.get('734188152037572608').send("<@246190853096341504> and <@254832481407074304> it is time to bump your server! Please type !d bump.");
+    
+}
+//export {SetTimer};
+module.exports = {SetTimer: SetTimer, sendMessage: sendMessage};
